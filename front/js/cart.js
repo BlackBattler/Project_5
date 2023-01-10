9bbb627c2ffa48e6ba694261cfe027ebd22a3911
Zedtai <coquin.atthaphon@outlook.com>
2023-01-10 18:47:43 +0100

    supp item

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,13 +6,9 @@
 // Fonction principale
 // --------------------------------------------------
 
-const monPanier = fGetPanier();
-console.log(monPanier);
+let monPanier = fGetPanier();
 
-(function () {
-    monPanier.forEach((dataPanier) => fDisplayPanier(dataPanier))
-
-})()
+monPanier.forEach((dataPanier) => fDisplayPanier(dataPanier));
 
 function fGetPanier() {
     const dataPanier = localStorage.getItem("cart");
@@ -21,6 +17,10 @@ function fGetPanier() {
     } else {
         return JSON.parse(dataPanier);
     }
+}
+
+function fSavePanier(dataPanier) {
+    localStorage.setItem("cart", JSON.stringify(dataPanier));
 }
 
 // --------------------------------------------------
@@ -134,7 +134,10 @@ function fSettingDelete(settings, dataPanier) {
     divDelete.classList.add("cart__item__content__settings__delete");
 
     const supprimer = document.createElement("p");
+    supprimer.classList.add("deleteItem");
     supprimer.textContent = "Supprimer";
+    supprimer.addEventListener("click", () => fDeleteItem(dataPanier.id, dataPanier.color));
+
     divDelete.appendChild(supprimer);
     settings.appendChild(divDelete);
 }
@@ -154,136 +157,26 @@ function fDisplayTotalPrice() {
     totalPrice.textContent = total;
 }
 
-// --------------------------------------------------
-// Fonction Changement de Quantite
-// --------------------------------------------------
-function updateQuantityAndPrice(dataPanier) {
-    const panier = document.querySelector(".cart__item");
-
-    panier.forEach((panier) => {
-        panier.addEventListener("change", (eq) => {
-            for (article of dataPanier)
-                if(article._id === panier.dataset.id && cart.dataset.couleur === article.couleur) {
-                    article.qu
-                }
-        })
-    })
-
-
-    fDisplayTotalQuantity();
-    fDisplayTotalPrice();
-}
-
-function fSavePanier(dataPanier) {
-    localStorage.setItem("cart", JSON.stringify(dataPanier));
-}
-
 
 // --------------------------------------------------
-// Formulaire de commande
+// Fonction Changement de quantite
 // --------------------------------------------------
 
-// Declaration Variables
-let firstNameCheck = false;
-let lastNameCheck = false;
-let addressCheck = false;
-let cityCheck = false;
-let emailCheck = false;
-let addressRegExp = new RegExp("^[0-9]{1,5}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+");
-let emailRegExp = new RegExp("^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,10}$");
-let charRegExp = new RegExp("^[a-zA-Z ,.'-]+$");
+// --------------------------------------------------
+// Fonction de suppression d'article
+// --------------------------------------------------
 
-// --------------------------------------------------
-// Fonction de verification de saisie
-// --------------------------------------------------
-function inputCheck(input, regExp, errorMsg, msg) {
-    if (regExp.test(input.value)) {
-        errorMsg.innerHTML = "";
-    }
-    else {
-        errorMsg.innerHTML = "Veuillez saisir " + msg;
-    }
-}
+function fDeleteItem(itemID, itemColor, dataPanier) {
+    console.log("delete");
+    let numTrouve;
 
-// --------------------------------------------------
-// Verification des champs du formulaire
-// --------------------------------------------------
-firstName.addEventListener("change", () => {
-    inputCheck(firstName, charRegExp, firstNameErrorMsg, "un prénom");
-    if (firstNameErrorMsg.innerHTML != '') {
-        firstNameCheck = false;
-    } else {
-        firstNameCheck = true;
-    }
-})
-lastName.addEventListener("change", () => {
-    inputCheck(lastName, charRegExp, lastNameErrorMsg, "un nom");
-    if (lastNameErrorMsg.innerHTML != '') {
-        lastNameCheck = false;
-    } else {
-        lastNameCheck = true;
-    }
-})
-address.addEventListener("change", () => {
-    inputCheck(address, addressRegExp, addressErrorMsg, "une adresse (Exemple : 10 quai de la charente)");
-    if (addressErrorMsg.innerHTML != '') {
-        addressCheck = false;
-    } else {
-        addressCheck = true;
-    }
-})
-city.addEventListener("change", () => {
-    inputCheck(city, charRegExp, cityErrorMsg, "une ville (Exemple : Paris)");
-    if (cityErrorMsg.innerHTML != '') {
-        cityCheck = false;
-    } else {
-        cityCheck = true;
-    }
-})
-email.addEventListener("change", () => {
-    inputCheck(email, emailRegExp, emailErrorMsg, "un e-mail (Exemple : kanap@gmail.com)");
-    if (emailErrorMsg.innerHTML != '') {
-        emailCheck = false;
-    } else {
-        emailCheck = true;
-    }
-})
-
-// --------------------------------------------------
-// Bouton de validation de commande
-// --------------------------------------------------
-order.addEventListener("click", (event) => {
-    if (firstNameCheck == true && lastNameCheck == true && addressCheck == true && cityCheck == true && emailCheck == true && panier.length != 0) {
-        let produitIDArray = [];
-        for (let i = 0; i < dataPanier.length; i++) {
-            produitIDArray.push(dataPanier[i].id);
+    
+    for (let i = 0; i < monPanier.lenght; i++) {
+        if (monPanier[i].id == itemID && monPanier[i].color == itemColor) {
+            numTrouve = i;
+            break;
         }
-
-        let panierClient = {
-            contact: {
-                "firstName": firstName.value,
-                "lastName": lastName.value,
-                "address": address.value,
-                "city": city.value,
-                "email": email.value,
-            },
-            "products": produitIDArray,
-        };
-
-        fetch('http://localhost:3000/api/products/order', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(panierClient)
-        })
-
-            .then((res) => res.json())
-            .then((data) => {
-                document.location.href = `confirmation.html?orderId=${data.orderId}`;
-            })
-            .catch(err => {
-                console.log("Erreur de traitement de la commande")
-            })
     }
-})
+    monPanier.splice(numTrouve, 1);
+    fSavePanier(monPanier);
+}
